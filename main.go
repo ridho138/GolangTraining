ab48bd0abb1cb02ba09cf9ae2bc1dbcf88202a94
Teguh Ridho Afdiilla <teguh.afdilla@mnc-insurance.com>
2022-08-22 20:14:12 +0700

    buat slice

--- main.go
+++ main.go
@@ -4,11 +4,17 @@ import "fmt"
 
 func main() {
 
-	for i := 0; i <= 10; i++ {
-		if i%2 == 0 {
-			fmt.Printf("%d Genap\n", i)
-		} else {
-			fmt.Printf("%d Ganjil\n", i)
-		}
+	// for i := 0; i <= 10; i++ {
+	// 	if i%2 == 0 {
+	// 		fmt.Printf("%d Genap\n", i)
+	// 	} else {
+	// 		fmt.Printf("%d Ganjil\n", i)
+	// 	}
+	// }
+
+	var names = []string{"Kevin Hugo", "Kadek Bintang Anjasmara", "Guntur Satrya Saputro", "Achmad Fathoni", "Edwin Setya Noegroho", "Jaka Prima Maulana", "Stevanus Dewana", "Hans Parson", "Rizki Ramadhan", "Mochammad Zayyan Ramadhan"}
+
+	for i, s := range names {
+		fmt.Println(i+1, s)
 	}
 }
